71b3812ddcbfa17dce82420b4bf654fa2cbbe79c
O Jesas <jesaiassa@gmail.com>
2023-07-14 17:08:20 -0300

    Function menor dif de preços de venda com prejuizo

--- 005 Perda mínima/index.js
+++ 005 Perda mínima/index.js
@@ -2,12 +2,12 @@ let precos = [30, 10, 54, 76, 1, 4, 35];
 
 
 function solucao(precos) {
-    let menorDif = Math.max(...precos);
+    let menorDif = 99999999999;
     for (let i = 0; i < precos.length; i++) {
         for (let j = i + 1; j < precos.length; j++) {
             let difTemp = (precos[i] - precos[j]);
             menorDif = (difTemp < menorDif) && (difTemp > 0) ? difTemp : menorDif;
-
+            
         }
 
     }
