8978019059a50bf08e6a26d91254886176bf3a4c
O Jesas <jesaiassa@gmail.com>
2023-07-14 21:41:44 -0300

    Finish function solucao

--- 006/index.js
+++ 006/index.js
@@ -1,5 +1,25 @@
 
+let palavras = ["aveia", "manha", "ave"];
+let primeiraLetra = "a";
+let segundaLetra = "v";
+
+
 function solucao(primeiraLetra, segundaLetra, palavras) {
-	//seu codigo aqui
+	let saida = null;
+	let cont = 0;
+	for (let palavra of palavras) {
+
+		if ((palavra[0] === primeiraLetra) && (palavra[1] === segundaLetra)) {
+			console.log(palavra);
+			cont++;
+		} else {
+			saida = "NENHUMA";
+		}
+	}
+	if (cont === 0) {
+		console.log(saida);
+	}
 
 }
+
+solucao(primeiraLetra, segundaLetra, palavras);
