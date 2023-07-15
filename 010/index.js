45ab86937522999ffe9ac541bc9660d0cbc08c57
O Jesas <jesaiassa@gmail.com>
2023-07-14 23:16:31 -0300

    Finish function solucao

--- 010/index.js
+++ 010/index.js
@@ -2,13 +2,9 @@ let resultados = ["V", "E", "V", "E"];
 
 function solucao(resultados) {
     let saida = 0;
-
     for (let resultado of resultados) {
-        if (resultado === "V") {
-            saida += 3;
-        } else if (resultado === "E") {
-            saida += 1;
-        }
+        if (resultado == "V") {saida += 3;}
+        else if (resultado == "E") {saida++;}
     }
     console.log(saida);
 }
